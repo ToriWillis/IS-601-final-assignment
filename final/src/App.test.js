3de3d5a2044803ec0907c941c1620288abe2c5b5
Tori Willis <twillis75@stlcc.edu>
2022-07-30 19:59:03 -0500

    Updated Week8

--- final/src/App.test.js
+++ final/src/App.test.js
@@ -9,17 +9,11 @@ beforeEach(() => {
   Pokedex.mockReturnValue({
     resource: jest.fn().mockResolvedValue({
       results: [{ name: 'national' }]
-    })
-  })
-  Pokedex.prototype.getPokedexByName = jest.fn(() => {
-    console.log(" Get Pokedex By Name")
-    Promise.resolve({
+    }),
+    getPokedexByName: jest.fn().mockResolvedValue({
       pokemon_entries: [{ entry_number: 1, pokemon_species: { name: "bulbasaur" } }],
-    })
-});
-  Pokedex.prototype.getPokemonByName = jest.fn(() => {
-    console.log("Get Pokemon By Name")
-    Promise.resolve({
+    }),
+    getPokemonByName: jest.fn().mockResolvedValue({
       abilities: [{ ability: { name: "overgrow" } }],
       name: "bulbasaur",
       types: [{ type: { name: "grass" } }],
@@ -28,9 +22,9 @@ beforeEach(() => {
           "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
       },
       stats: [{ base_stat: 49, stat: { name: "attack" } }],
-    })
+    }),
+  });
 });
-})
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -38,6 +32,6 @@ afterEach(() => {
 
 test("A", async () => { render(<App />); await screen.findByText("national") })
 test("B", async () => { render(<App />); fireEvent.click(await screen.findByText("View")); await screen.findByText("bulbasaur") })
-test("C", async () => { render(<App />); fireEvent.click(await screen.findByText("View")); fireEvent.click(await screen.findByText("View")); await screen.findByText("bulbasaur"); expect(screen.getByText("overgrow")).toBeInTheDocument();expect(screen.getByText("attack")).toBeInTheDocument();expect(screen.getByText("grass")).toBeInTheDocument();expect(screen.getByText("49")).toBeInTheDocument(); })
+test("C", async () => { render(<App />); fireEvent.click(await screen.findByText("View")); fireEvent.click(await screen.findByText("View")); await screen.findByText("bulbasaur"); expect(screen.getByText("overgrow")).toBeInTheDocument(); expect(screen.getByText("attack: 49")).toBeInTheDocument(); expect(screen.getByText("grass")).toBeInTheDocument(); })
 
 
